34d28fb25e7f19c5920ce5db4fcead38c273598c
AndSinhorin <andressasinhorin@gmail.com>
2024-10-16 16:27:25 -0300

    atualizado

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    mongoose
-        .connect('mongodb://localhost:27017/oficina') 
+    mongoose.connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
+        ) 
         .then(() => console.log('Connected to MongoDB')) 
         .catch((error) => console.log(error)); 
 };
+
+connectDB()
 
 export default connectDB;
